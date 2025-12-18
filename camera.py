32c2b67853b5721aad6264ba95e5e403bf7abd6c
Nica Robertson <nicagrace05@gmail.com>
2025-12-17 23:12:00 -0800

    grabbing frames from camera working

--- camera.py
+++ camera.py
@@ -5,8 +5,16 @@ if not cap.isOpened() :
     raise RuntimeError('couldnt open camera')
 
 while True :
-    ret, frame = cap.read()
+    ret, frame = cap.read() #frame is HxWx3, for BGR channels, vals from 0-255
     if not ret : 
+        print('failed to get frame')
         break
-    cv2.imshow('camera')
+    cv2.imshow('Camera', frame) #creates window to display current array
+    if cv2.waitKey(1) & 0xFF == ord('q') : 
+        break #exits loop when press q
+
+cap.release() #returning the hardware 
+cv2.destroyAllWindows()
+
+
     
