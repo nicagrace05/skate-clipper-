91b615554c2ac7f397ede1170e4bd241b29b20f9
Nica Robertson <nicagrace05@gmail.com>
2025-12-19 00:27:22 -0800

    fixed framing, typos

--- camera.py
+++ camera.py
@@ -12,30 +12,28 @@ while True :
     if not ret : 
         print('failed to get frame')
         break
-    
-    if cv2.waitKey(1) & 0xFF == ord('q') : 
-        break #exits loop when press q
 
         #MOTION TRACKING
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #motion is soley about intensity here
     gray_blur = cv2.GaussianBlur(gray, (21, 21), 0) #big blur to filter out noise
-    first_frame = None #everything realtive to empty garage
     if first_frame is None:
         first_frame = gray_blur
         continue 
     frame_delta = cv2.absdiff(first_frame, gray_blur) #the differences in frames visualized in pixels (white=motion)
     _, thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY) #defining what is considered movement
-    thresh = cv2.dialate(thresh, None, iterations = 2)
-    contours, _, = cv2.findContours(
-        thresh.copy(),
-        cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE
-    ) #contours closed loop aaroun white areas 
-    (x,y,w,h) = cv2.boundingRect(c)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255), 2)
+    thresh = cv2.dilate(thresh, None, iterations = 2)
+    contors, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)#contours closed loop aaroun white areas 
+    for c in contors :
+        if cv2.contourArea(c) < 7000 :
+            continue 
+        (x,y,w,h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255, 0), 2)
 
-    v2.imshow('Camera', frame) #creates window to display current array
-    v2.imshow('Motion Mask', thresh) #shows framing window 
+    cv2.imshow('Camera', frame) #creates window to display current array
+    cv2.imshow('Motion Mask', thresh) #shows framing window 
+
+    if cv2.waitKey(1) & 0xFF == ord('q') : 
+        break #exits loop when press q
 
 cap.release() #returning the hardware 
 cv2.destroyAllWindows()
